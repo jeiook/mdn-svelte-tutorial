1ddbcdcee268da1a8348c8b32c7bc74160196546
Jewook Ryu <11579865+jeiook@users.noreply.github.com>
2024-03-19 19:52:03 -0700

    Todo component + custom remove event/handler

---
+++ 02-starting-our-todo-app/src/components/Todo.svelte
@@ -0,0 +1,31 @@
+<script>
+  import { createEventDispatcher } from "svelte";
+
+  export let todo;
+
+  const dispatch = createEventDispatcher();
+</script>
+
+<div class="stack-small">
+  <div class="c-cb">
+    <input
+      type="checkbox"
+      id="todo-{todo.id}"
+      on:click={() => (todo.completed = !todo.completed)}
+      checked={todo.completed}
+    />
+    <label for="todo-{todo.id}" class="todo-label">{todo.name}</label>
+  </div>
+  <div class="btn-group">
+    <button type="button" class="btn">
+      Edit <span class="visually-hidden">{todo.name}</span>
+    </button>
+    <button
+      type="button"
+      class="btn btn__danger"
+      on:click={() => dispatch("remove", todo)}
+    >
+      Delete <span class="visually-hidden">{todo.name}</span>
+    </button>
+  </div>
+</div>
