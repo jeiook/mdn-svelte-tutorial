f68b9d0f1fdd20a364e7554c89be707a27f64b2a
Jewook Ryu <11579865+jeiook@users.noreply.github.com>
2024-03-21 15:31:03 -0700

    Local storage store for todos

--- 02-starting-our-todo-app/src/App.svelte
+++ 02-starting-our-todo-app/src/App.svelte
@@ -2,12 +2,8 @@
   import Alerts from "./components/Alerts.svelte";
   import Todos from "./components/Todos.svelte";
 
-  let todos = [
-    { id: 1, name: "Create a Svelte starter app", completed: true },
-    { id: 2, name: "Create your first component", completed: true },
-    { id: 3, name: "Create the rest of the tutorial", completed: false },
-  ];
+  import { todos } from "./stores.js";
 </script>
 
 <Alerts />
-<Todos {todos} />
+<Todos bind:todos={$todos} />
