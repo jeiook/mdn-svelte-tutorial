42459e8b373b7f31c791c6fe763eac1d0ca1ffe4
Jewook Ryu <11579865+jeiook@users.noreply.github.com>
2024-03-25 13:48:27 -0700

    Progress bar with special styling for 100%

--- todo-app-typescript/src/components/TodoStatus.svelte
+++ todo-app-typescript/src/components/TodoStatus.svelte
@@ -5,7 +5,9 @@
 
 	$: totalTodos = todos.length;
 	$: completedTodos = todos.filter((t) => t.completed).length;
+	$: percentage = Math.round((completedTodos / totalTodos) * 100);
 
+	let progressBarBoxWidth: number;
 	let headingEl: HTMLElement;
 
 	export function focus() {
@@ -13,6 +15,35 @@
 	}
 </script>
 
+{#if completedTodos > 0}
+	<div class="progress-bar">
+		<div
+			bind:clientWidth={progressBarBoxWidth}
+			class={'box' + (completedTodos === totalTodos ? ' complete' : '')}
+			style="--percentage:{percentage}"
+		>
+			<p>
+				{#if progressBarBoxWidth >= 40}
+					{percentage}%
+				{/if}
+				{#if progressBarBoxWidth >= 125}
+					<span>Completed</span>
+				{/if}
+			</p>
+		</div>
+	</div>
+{/if}
 <h2 id="list-heading" bind:this={headingEl} tabindex="-1">
 	{completedTodos} out of {totalTodos} items completed
 </h2>
+
+<style>
+	.progress-bar .box {
+		width: calc(var(--percentage) * 1%);
+	}
+
+	.progress-bar .complete {
+		background-color: gold;
+		color: black;
+	}
+</style>
